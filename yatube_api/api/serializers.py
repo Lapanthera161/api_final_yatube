a37e8b12021d3893ef7f4227cb8fe7c5c2036c2f
Catherina Govorova <ei.govorova@yandex.ru>
2022-04-03 16:15:09 +0300

    1

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,7 +1,6 @@
-from posts.models import Comment, Follow, Group, Post, User # isort:skip
+from posts.models import Comment, Follow, Group, Post, User   # isort:skip
 
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
 
 
 class PostSerializer(serializers.ModelSerializer):
