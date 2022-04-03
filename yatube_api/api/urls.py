5bdc52946e9ab9f27ce2845984c4cda24520fffc
Catherina Govorova <ei.govorova@yandex.ru>
2022-04-03 16:35:00 +0300

    1

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,33 +1,23 @@
 from django.urls import include, path
 
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView, TokenVerifyView)
+                                            TokenRefreshView)
 
-from .views import (CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet,
-                    UserViewSet)
+from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
-router = routers.DefaultRouter()
-router.register('posts', PostViewSet)
-router.register('groups', GroupViewSet)
-router.register('users', UserViewSet)
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='posts')
 router.register('follow', FollowViewSet, basename='follow')
-router.register(r'posts/(?P<post_id>\d+)/comments',
-                CommentViewSet, basename='post_comments'
-                )
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
+                basename='comments')
+router.register('groups', GroupViewSet)
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
-    path(
-        'v1/jwt/create/', TokenObtainPairView.as_view(),
-        name='token_obtain_pair'
-    ),
-    path(
-        'v1/jwt/refresh/', TokenRefreshView.as_view(),
-        name='token_refresh'
-    ),
-    path(
-        'v1/jwt/verify/', TokenVerifyView.as_view(),
-        name='token_verify'
-    ),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.jwt')),
+    path('api/v1/', include(router.urls)),
+    path('v1/auth/', include('djoser.urls')),
+    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/token/refresh/', TokenRefreshView.as_view, name='token_refresh')
 ]
